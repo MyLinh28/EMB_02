ec51dc5e63b3e5f4a2134c9c0cceb625d3587e01
Linh <trinhmylinh28@gmail.com>
2020-04-13 15:13:51 +0700

    master exit

---
+++ sort.c
@@ -0,0 +1,59 @@
+/*********************************************************
+Decription: sorted array ascending, descending
+**********************************************************/
+#include <stdio.h>
+
+void up(int *a, int n)
+{
+	   int i, j;
+	   for(i=0; i<n; i++)
+	   {
+	   	    for(j=i+1; j<n; j++)
+	   	    {
+	   	    	    if(*(a+i) > *(a+j))
+	   	    	    {
+	   	    	    	    int temp = *(a+i);
+	   	    	    	    *(a+i) = *(a+j);
+	   	    	    	    *(a+j) = temp;
+														}
+									}
+				}
+}
+
+void down(int *a, int n)
+{
+	   int i, j;
+	   for(i=0; i<n; i++)
+	   {
+	   	    for(j=i+1; j<n; j++)
+	   	    {
+	   	    	    if(*(a+i) < *(a+j))
+	   	    	    {
+	   	    	    	    int temp = *(a+i);
+	   	    	    	    *(a+i) = *(a+j);
+	   	    	    	    *(a+j) = temp;
+														}
+									}
+				}
+}
+
+void out(int *a, int n)
+{
+	   int i;
+	   for(i=0; i<n; i++)
+	   {
+	   	    printf("%d ", *(a+i));
+				}
+				printf("\n");
+}
+
+int main()
+{
+	   int a[10] = {1, 34, 47, 5, 23, 12, 8, 4, 32, 9};
+	   out(a, 10);
+	   up(a, 10);
+	   out(a, 10);
+	   down(a, 10);
+	   out(a, 10);
+	   return 0;
+}
